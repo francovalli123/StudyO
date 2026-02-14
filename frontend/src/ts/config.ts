04967b57cd8948c4097dc2616b2c9973a7a99828
francovalli123 <frankovalli123@gmail.com>
2026-02-13 23:17:21 -0300

    Fetch error solved

--- frontend/src/ts/config.ts
+++ frontend/src/ts/config.ts
@@ -1,5 +1,5 @@
 export const API_URL =
   window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
     ? "http://127.0.0.1:8000"
-    : "https://studyo-backend.onrender.com";
+    : "https://studyo.onrender.com";
 
